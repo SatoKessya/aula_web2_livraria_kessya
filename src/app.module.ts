a901b171525fd767d963052f14888185d36e10d9
SatoKessya <Kessya.sato@icloud.com>
2026-04-07 22:34:29 -0300

    Configuaracao do ORM Drizzle

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AutoresModule } from './modules/autores/autores.modulel';
-
-@Module({
-  imports: [AutoresModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
