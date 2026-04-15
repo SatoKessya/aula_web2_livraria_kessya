b459568d7955fb51dee1f642f4a2f22e4960103a
SatoKessya <Kessya.sato@icloud.com>
2026-04-14 22:44:35 -0300

    feat/ configuração do ORM pronta para ser aplicada na nossa aplicação e ser consumida dentro dos repository

---
+++ src/db/database/database.module.ts
@@ -0,0 +1,37 @@
+import { Global, Module } from '@nestjs/common';
+import { DATABASE_URL, DRIZZLE } from './database.constants';
+import { drizzle } from 'drizzle-orm/node-mssql';
+import { connect } from 'mssql';
+
+import type { config as MsSqlConfig } from 'mssql';
+import * as schema from '../schemas/index';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE,
+      inject: [],
+      useFactory: async () => {
+        const dbConfig: MsSqlConfig = {
+          server: 'SRV-BD-1',
+          port: 1433,
+          user: 'alunos_des225',
+          password: '123',
+          database: 'des225_kessya',
+          options: {
+            encrypt: false,
+            trustServerCertificate: true,
+          },
+        };
+
+        const pool = await connect(dbConfig);
+
+        return drizzle({ client: pool, schema: schema });
+      },
+    },
+  ],
+
+  exports: [DRIZZLE],
+})
+export class DatabaseModule {}
